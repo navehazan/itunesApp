bc1deb6c2cf18ad3080249df7920e44612cc8071
navehazan <nave@cream-software.com>
2018-12-15 23:56:13 +0200

    fix border radius

--- src/components/ResultsListItem.js
+++ src/components/ResultsListItem.js
@@ -3,16 +3,14 @@ import { Link } from "react-router-dom";
 const ResultsListItem = ({ track }) => (
   <div className="ResultsListItem">
       <img
-        className="TrackDetails-image"
+        className="ResultsListItem-image"
         src={track.artworkUrl100}
         alt="Track"
       />
-      <div className="TrackDetails-info">
         <p>
           <span className="title">Track Name </span> <br />
           {track.trackName}
         </p>
-      </div>
       {track.trackId && (
         <Link className="link" to={`/info/${track.trackId}`}>
           info
